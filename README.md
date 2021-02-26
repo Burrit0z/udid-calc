d5fa9d139272dbc56bca552b9d3bf2d1a1488a1c
burrit0z <carsondzielinski@gmail.com>
2021-02-25 19:10:16 -0500

    Update to use dlsym for obtaining function pointers

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # udid-calc
 Calculate UDID manually from MobileGestalt
+Does **not** require linking against libMobileGestalt
 
-If you just want to get the UDID with no extra steps, literally just use the following line and add MobileGestalt lib to your project:
+Please see main.m
+
+If you just want to get the UDID with no extra steps, literally just use the following line. You will have to link your project against libMobileGestalt:
 ```objc
 NSString *UDID = (NSString *)CFBridgingRelease((CFStringRef)MGCopyAnswer(CFSTR("UniqueDeviceID")));
 ```
-
-If you want to manually fetch the values from Gestalt and calculate yourself, use the contents of main.m.
-This requires libMobileGestalt, and for lower than A12, you'll need the Security framework.
--- main.m
+++ main.m
@@ -1,16 +1,37 @@
+//
+//  main.m
+//  udid-calc
+//
+//  Created by burrit0z on 2/25/21.
+//
+
+// POC manual udid calculation. This source file is not intended to be compiled,
+// ever. Just copy the imports and code inside the getUDID function into your
+// project.
+
+#include <dlfcn.h>
 #import <Foundation/Foundation.h>
 #if !(__arm64e__)
 #import <CommonCrypto/CommonDigest.h>
 #endif
 
+// Don't make it a function like this. Make it static and inline at the very
+// minimum. Otherwise, one can dlopen your executable and use MobileSubstrate to
+// very easily hook it, and all our work is for nothing.
 NSString *getUDID() {
-
-    // Please note, this will not ensure the UDID you get is untampered with!
-    // This is simply a way to calculate it manually, and using this over the one liner
-    // does not mean it's more secure. It is just an alternative way.
 
     // All of this below is equivalent to the one line:
     // NSString *UDID = (NSString *)CFBridgingRelease((CFStringRef)MGCopyAnswer(CFSTR("UniqueDeviceID")));
+    // However, it does require linking agaist libMobileGestalt
+    // The code below does not require linking against anything additional.
+
+    // Use dlopen to get a handle on libMobileGestalt
+    // This helps ensure we get the real function pointer so when we call it
+    // we don't get a hooked version
+    void *gestalt_handle = dlopen("/usr/lib/libMobileGestalt.dylib", RTLD_NOW);
+    if(gestalt_handle == NULL) return;
+    CFPropertyListRef (*MGCopyAnswer)(CFStringRef property) = (CFPropertyListRef(*)(CFStringRef property))dlsym(gestalt_handle, "MGCopyAnswer");
+    if(MGCopyAnswer == NULL) return;
 
     #if __arm64e__
 
@@ -26,6 +47,14 @@ NSString *getUDID() {
     NSString *secret = [NSString stringWithFormat:@"%@-%@", chipid, ecid];
 
     #else
+
+    // On A11 and below, we have to use Security framework for CC_SHA1 (SHA1)
+    // Same idea here, just dlopen it in order to get the function pointer
+    // Enjoy the lovely one liner with the cast.
+    void *security_handle = dlopen("/System/Library/Frameworks/Security.framework/Security", RTLD_NOW);
+    if(security_handle == NULL) return;
+    void (*CC_SHA1)(const void *data, long len, unsigned char *md) = (void (*)(const void *data, long len, unsigned char *md))dlsym(security_handle, "CC_SHA1");
+    if(CC_SHA1 == NULL) return;
 
     // A11 and lower UDID = SHA1(serial + ecid + wifiAddress + bluetoothAddress)
     NSString *serial = (NSString *)CFBridgingRelease((CFStringRef)MGCopyAnswer(CFSTR("SerialNumber")));
@@ -54,6 +83,7 @@ NSString *getUDID() {
 
 }
 
+// Obviously you would not have main in a tweak.
 int main() {
     NSLog(@"UDID: %@", getUDID());
     return 0;
